23dc1e145b692700264ea41146cd0380827e3bdc
Aman Babu Gautam <amanbabugautam@gmail.com>
2016-07-22 01:18:38 +0530

    celery added

---
+++ reminderapp/reminderapp/config/celery.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+from django.conf import settings  # noqa
+from datetime import timedelta
+import random
+
+app = Celery('config')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+app.conf.CELERY_TIMEZONE = 'UTC'
