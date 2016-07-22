cf9eebe74aac552d084e5a437c7bca2e37bee9b5
Aman Babu Gautam <amanbabugautam@gmail.com>
2016-07-22 13:46:42 +0530

    validators added on email and phone fields

--- reminderapp/reminderapp/apps/reminder_web_app/models.py
+++ reminderapp/reminderapp/apps/reminder_web_app/models.py
@@ -1,14 +1,39 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.translation import ugettext as _
-
+from django.core import validators
 from datetime import date
+
 # Create your models here.
+
 class Reminder(models.Model):
 	date = models.DateField(_("Date"), default=date.today)
 	time = models.TimeField(_("Time"))
-	email_id = models.EmailField(_("Email Address"),blank=True, null=True)
-	phone_no = models.CharField(_("Phone no."),max_length=10, blank=True, null=True)
+	email_id = models.EmailField(
+				_("Email Address"),
+				blank=True,
+				null=True,
+				help_text=('Valid Email Address'),
+				validators=[
+            				validators.RegexValidator(
+                			r'^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$',
+                			_('Enter a valid email address')
+            				),
+        		],
+	)
+	phone_no = models.CharField(
+				_("Phone no."),
+				max_length=10,
+				blank=True, 
+				null=True,
+				help_text=('A valid Phone No.'),
+				validators=[
+            				validators.RegexValidator(
+                			r'^([7|8|9]\d{9})$',
+                			_('Enter a valid Phone Number')
+            				),
+        		],
+	)
 	message = models.CharField(_("Reminder Message"), max_length=50)
 
 	class Meta:
@@ -17,4 +42,4 @@ class Reminder(models.Model):
 		verbose_name_plural = 'reminder_tables'
 
 	def __unicode__(self):
-		return '%s' % self.email_id
+		return self.email_id
