72135204d5458a4fd263707777d7ff1930c5b816
Aman Babu Gautam <amanbabugautam@gmail.com>
2016-07-22 15:53:46 +0530

    views added

--- reminderapp/reminderapp/apps/reminder_web_app/views.py
+++ reminderapp/reminderapp/apps/reminder_web_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from reminder_web_app.models import Reminder
+from reminder_web_app.serializers import ReminderSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class ReminderViewSet(viewsets.ModelViewSet):
+	queryset = Reminder.objects.all()
+	serializer_class = ReminderSerializer
