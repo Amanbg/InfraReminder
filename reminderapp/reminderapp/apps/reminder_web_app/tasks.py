6717c54c64278a1ccc8ae80fd6d73116641384ae
Aman Babu Gautam <amanbabugautam@gmail.com>
2016-07-24 19:28:47 +0530

    add tasks for reminder

--- reminderapp/reminderapp/apps/reminder_web_app/tasks.py
+++ reminderapp/reminderapp/apps/reminder_web_app/tasks.py
@@ -1,39 +1,53 @@
 from celery import Task
-from reminder_web_app import models
-# from reminder_web_app.models import Reminder
+from reminder_web_app.models import Reminder
 from datetime import datetime, timedelta
 from config.celery import app
 from django.core.exceptions import ValidationError
 from datetime import datetime
 import time
 
-count = 1
+count =1
 
 @app.task
-def createReminder(count):
+def createReminder(reminder_id):
+    
     try:
         reminder_object = None
         while reminder_object is None:
             try:
-                reminder_object = Reminder.object.get(id=count)
+                reminder_object = Reminder.objects.get(id=reminder_id)
             except:
-                count = count +1
+                pass
 
-        offset = datetime.utcnow() - datetime.now()
+        s1 = str(reminder_object.date)+" "+str(reminder_object.time)
+        # print "s1 : ",s1
+        s2 = datetime.now()
+        s2 = s2.strftime("%Y-%m-%d %H:%M:%S")
+        # print "s2 : ",s2
+        FMT = "%Y-%m-%d %H:%M:%S"
+        tdelta = datetime.strptime(s1,FMT) - datetime.strptime(s2,FMT)
+        # if tdelta.days < 0:
+        #     print "tdelta < 0",tdelta.days
+        #     tdelta = timedelta(days=0,
+        #         seconds=tdelta.seconds)
 
-        remind_time = reminder_object.date + " " + reminder_object.time
-        print "remind_time :",remind_time
+        # print "tdelta : ", tdelta
+        result_utc = datetime.utcnow() + timedelta(seconds=tdelta.seconds)
         
-        localtime = datetime.strptime(remind_time,"%Y-%m-%d %H:%M:%S")
-        
-        result_utc = localtime+offset
+        result_utc = result_utc.replace(microsecond=0)
+        # print "result_utc : ",result_utc
 
-        if result_utc==datetime.utcnow():
+        current_utc = datetime.utcnow().replace(microsecond=0)
+        
+        # print "current utc : ",current_utc
+        if current_utc < result_utc:
+            pass
+        elif current_utc==result_utc:
             print"Reminder Ring Up.. "
             reminder_object.reminder_received=True
             reminder_object.save()
 
-        createReminder(count).apply_async(eta=result_utc)
+        createReminder.apply_async((reminder_id,),eta=result_utc)
         return True
     except Exception as e:
         print str(e)
